89c46e12fdc95a85a208af7beadd426356da6d49
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-03-12 16:57:35 -0300

    classe entity, construtores e getters/setters para os atributos. TTL do mongo para o atributo expiresAt.

---
+++ src/main/java/com/lorenzo/urlshortener/entities/UrlEntity.java
@@ -0,0 +1,51 @@
+package com.lorenzo.urlshortener.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "urls")
+public class UrlEntity {
+    @Id
+    private String id;
+
+    private String fullUrl;
+
+    @Indexed(expireAfter = "60s")
+    private LocalDateTime expiresAt;
+
+    public UrlEntity(String id, String fullUrl, LocalDateTime expiresAt) {
+        this.id = id;
+        this.fullUrl = fullUrl;
+        this.expiresAt = expiresAt;
+    }
+
+    public UrlEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullUrl() {
+        return fullUrl;
+    }
+
+    public void setFullUrl(String fullUrl) {
+        this.fullUrl = fullUrl;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
