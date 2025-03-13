feb6fe9d6138363917f423b733e2cb1a821bd851
Lorenzo Rodrigues <lorenzocostarodrigues10@gmail.com>
2025-03-13 07:13:48 -0300

    endpoint redirect que redireciona o usuário com a url encurtada para a url original

--- src/main/java/com/lorenzo/urlshortener/controller/UrlController.java
+++ src/main/java/com/lorenzo/urlshortener/controller/UrlController.java
@@ -6,12 +6,14 @@ import com.lorenzo.urlshortener.entities.UrlEntity;
 import com.lorenzo.urlshortener.repository.UrlRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.text.RandomStringGenerator;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
 public class UrlController {
@@ -38,5 +40,18 @@ public class UrlController {
         var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url",id);
 
         return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Void> redirect(@PathVariable("id")String id){
+        Optional<UrlEntity> url = urlRepository.findById(id);
+
+        if (url.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(url.get().getFullUrl()));
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
     }
 }
